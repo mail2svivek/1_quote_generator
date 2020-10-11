0dd550e28a2043f7e405eb613f401661ba2d1bb0
Vivek Singh <mail2svivek@gmail.com>
2020-10-11 16:16:40 +0530

    changes

--- script.js
+++ script.js
@@ -5,26 +5,47 @@ const quoteText=document.getElementById('quote')
 const authorName=document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loaderEle = document.getElementById('loader')
+
+//Show loading
+function loading(){
+ loaderEle.hdden=false
+ quoteContainer.hidden=true
+}
+
+
+//Hide Loading
+
+function complete(){
+ if(!loaderEle.hideen){
+  quoteContainer=false
+  loaderEle.hdden=true
+ }
+}
 
 async function getQuoteForismatic(){
+ loading()
  const proxyUrl='https://cors-anywhere.herokuapp.com/'
  const apiUrl ='http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
  try {
- let quoteResp = await fetch(proxyUrl + apiUrl)
- let quoteJSON = await quoteResp.json()
- console.log(quoteJSON)
- 
- if (quoteJSON.quoteAuthor===''){
-  authorName.innerText="Unknown"
-   }else {
-   authorName.innerText = quoteJSON.quoteAuthor
-  }
-  quoteText.innerText = quoteJSON.quoteText
-  if (quoteJSON.quoteText.length > 50 ){
-   quoteText.classList.add('long-quote')
-  } else {
-   quoteText.classList.remove('long-quote')
-  }
+  let quoteResp = await fetch(proxyUrl + apiUrl)
+  let quoteJSON = await quoteResp.json()
+  console.log(quoteJSON)
+  
+  if (quoteJSON.quoteAuthor===''){
+   authorName.innerText="Unknown"
+    }else {
+    authorName.innerText = quoteJSON.quoteAuthor
+   }
+   
+   if (quoteJSON.quoteText.length > 50 ){
+    quoteText.classList.add('long-quote')
+   } else {
+    quoteText.classList.remove('long-quote')
+   }
+   quoteText.innerText = quoteJSON.quoteText
+   //Stop loader and call complete It will hide the loader and show the container
+   complete()
  } catch(error){
   //getQuoteForismatic()
   console.log('whoops, no quote', error)
@@ -48,3 +69,4 @@ newQuoteBtn.addEventListener('click', getQuoteForismatic)
 //Onload
 
 getQuoteForismatic()
+// loading()
