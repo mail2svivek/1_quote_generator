632e25274dd56156202a7172754eb1ebda9b0603
Vivek Singh <mail2svivek@gmail.com>
2020-10-11 15:28:58 +0530

    changes

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('testing:')
+
+// Get Quote from API
+
+
+async function getQuoteForismatic(){
+ const proxyUrl='https://cors-anywhere.herokuapp.com/'
+ const apiUrl ='http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+ try {
+ let quoteResp = await fetch(proxyUrl + apiUrl)
+ let quoteJSON = await quoteResp.json()
+ console.log(quoteJSON)
+ } catch(error){
+  getQuoteForismatic()
+  console.log('whoops, no quote', error)
+ }
+
+}
+
+
+//Onload
+
+getQuoteForismatic()
